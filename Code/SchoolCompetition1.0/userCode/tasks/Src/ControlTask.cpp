cf5246e77a9433c835768004b0f7d9f26da479f2
Xhd <WilliamX1@users.noreply.github.com>
2021-11-06 20:18:54 +0800

    2021.11.6

--- Code/SchoolCompetition1.0/userCode/tasks/Src/ControlTask.cpp
+++ Code/SchoolCompetition1.0/userCode/tasks/Src/ControlTask.cpp
@@ -11,6 +11,8 @@ void CtrlHandle(){
     }else {//其他正常模式
         switch (RemoteControl::rcInfo.sLeft) {
             case UP_POS://左侧一档
+								/* 右摇杆前后拨动控制灵敏性，右摇杆左右拨动控制灵敏性，左摇杆左右拨动控制灵敏性 */
+								/* * 后的是一个系数，可以调节到最舒适的手感 */
                 ChassisSetVelocity(RemoteControl::rcInfo.right_col*4.2,
                                    RemoteControl::rcInfo.right_rol*4.2,RemoteControl::rcInfo.left_rol*60);
                 break;
